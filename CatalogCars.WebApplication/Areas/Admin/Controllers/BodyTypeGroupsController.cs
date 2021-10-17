aa63a99990555c2b9eb44e27193db5e7a559755a
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-10-17 17:51:30 +0500

    Реализовано отображение сущности "BodyTypeGroup" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/BodyTypeGroupsController.cs
@@ -0,0 +1,95 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodyTypeGroupsController : Controller
+    {
+        private readonly BodyTypeGroupsRequester _bodyTypeGroupsRequester;
+
+        public BodyTypeGroupsController()
+        {
+            _bodyTypeGroupsRequester = new BodyTypeGroupsRequester();
+        }
+
+        [HttpGet]
+        [Route("~/Admin/BodyTypeGroups")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new BodyTypeGroupsFilters();
+
+            var pagination = new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _bodyTypeGroupsRequester.GetCountBodyTypeGroupsAsync(filters)
+            };
+
+            var viewModel = new BodyTypeGroupsViewModel()
+            {
+                Filters = filters,
+                Pagination = pagination,
+                BodyTypeGroups = (await _bodyTypeGroupsRequester.GetBodyTypeGroupsAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/BodyTypeGroups")]
+        public async Task<PartialViewResult> Index([FromForm] BodyTypeGroupsViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+
+            viewModel.Pagination.NumberPage = viewModel.Filters.NumberPage;
+            viewModel.Pagination.ItemsPerPage = viewModel.Filters.ItemsPerPage;
+            viewModel.Pagination.CountTotalItems = await _bodyTypeGroupsRequester.GetCountBodyTypeGroupsAsync(viewModel.Filters);
+
+            viewModel.BodyTypeGroups = (await _bodyTypeGroupsRequester.GetBodyTypeGroupsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/BodyTypeGroups/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] BodyTypeGroupsViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+
+            viewModel.Pagination.NumberPage = viewModel.Filters.NumberPage;
+            viewModel.Pagination.ItemsPerPage = viewModel.Filters.ItemsPerPage;
+            viewModel.Pagination.CountTotalItems = await _bodyTypeGroupsRequester.GetCountBodyTypeGroupsAsync(viewModel.Filters);
+
+            viewModel.BodyTypeGroups = (await _bodyTypeGroupsRequester.GetBodyTypeGroupsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/BodyTypeGroups/Names")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _bodyTypeGroupsRequester.GetNamesBodyTypeGroupsAsync(searchString))
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+
+        [HttpPost]
+        [Route("~/Admin/BodyTypeGroups/ResetFilters")]
+        public PartialViewResult ResetFilters()
+        {
+            return PartialView("_Form", new BodyTypeGroupsViewModel() { Filters =new BodyTypeGroupsFilters() });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/BodyTypeGroupsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class BodyTypeGroupsViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public BodyTypeGroupsFilters Filters { get; set; }
+
+        public List<BodyTypeGroup> BodyTypeGroups { get; set; }
+    }
+}
