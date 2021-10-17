387bf1fb0ff37cddbe203adecf34fd9662eebf46
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-10-17 19:42:24 +0500

    Удаление лишнего.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using CatalogCars.Resource.Requests.HttpRequesters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly CategoriesRequester _categoriesRequester;
+
+        public CategoriesController()
+        {
+            _categoriesRequester = new CategoriesRequester();
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/CategoriesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class CategoriesViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public CategoriesFilters Filters { get; set; }
+
+        public List<Category> Categories { get; set; }
+    }
+}
