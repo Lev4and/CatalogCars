6c03242e5df5bdd5f8d9c74fa89a13422f31964c
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-10-20 21:08:42 +0500

    Реализовано отображение сущности "Color" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/ColorsController.cs
@@ -0,0 +1,86 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ColorsController : Controller
+    {
+        private readonly ColorsRequester _colorsRequester;
+
+        public ColorsController()
+        {
+            _colorsRequester = new ColorsRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(ColorsFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _colorsRequester.GetCountColorsAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/Colors")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new ColorsFilters();
+
+            var viewModel = new ColorsViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                Colors = (await _colorsRequester.GetColorsAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Colors")]
+        public async Task<PartialViewResult> Index([FromForm] ColorsViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Colors = (await _colorsRequester.GetColorsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Colors/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] ColorsViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Colors = (await _colorsRequester.GetColorsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Colors/Names")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _colorsRequester.GetNamesColorsAsync(searchString))
+                    .Append(searchString)
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/ColorsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class ColorsViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public ColorsFilters Filters { get; set; }
+
+        public List<Color> Colors { get; set; }
+    }
+}
