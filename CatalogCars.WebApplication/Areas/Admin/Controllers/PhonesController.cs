a6e0b19ff122acbb6fe29974a24690443600392f
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-10-21 21:39:07 +0500

    Реализовано отображение сущности "Phone" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/PhonesController.cs
@@ -0,0 +1,86 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PhonesController : Controller
+    {
+        private readonly PhonesRequester _phonesRequester;
+
+        public PhonesController()
+        {
+            _phonesRequester = new PhonesRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(PhonesFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _phonesRequester.GetCountPhonesAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/Phones")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new PhonesFilters();
+
+            var viewModel = new PhonesViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                Phones = (await _phonesRequester.GetPhonesAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Phones")]
+        public async Task<PartialViewResult> Index([FromForm] PhonesViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Phones = (await _phonesRequester.GetPhonesAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Phones/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] PhonesViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Phones = (await _phonesRequester.GetPhonesAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Phones/Values")]
+        public async Task<JsonResult> Values([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _phonesRequester.GetNamesPhonesAsync(searchString))
+                    .Append(searchString ?? "")
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/PhonesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class PhonesViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public PhonesFilters Filters { get; set; }
+
+        public List<Phone> Phones { get; set; }
+    }
+}
