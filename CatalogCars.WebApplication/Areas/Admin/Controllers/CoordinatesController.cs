4e9ec106753b8a50f1ad1ce2f2960167bedb865b
Андрей Левченко <a.levchenko@aviscloud.com>
2021-10-21 12:23:01 +0500

    Реализовано отображение сущности "Coordinate" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/CoordinatesController.cs
@@ -0,0 +1,107 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoordinatesController : Controller
+    {
+        private readonly RegionsRequester _regionsRequester;
+        private readonly CoordinatesRequester _coordinatesRequester;
+
+        public CoordinatesController()
+        {
+            _regionsRequester = new RegionsRequester();
+            _coordinatesRequester = new CoordinatesRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(CoordinatesFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _coordinatesRequester.GetCountCoordinatesAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/Coordinates")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new CoordinatesFilters();
+
+            var viewModel = new CoordinatesViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                Coordinates = (await _coordinatesRequester.GetCoordinatesAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Coordinates")]
+        public async Task<PartialViewResult> Index([FromForm] CoordinatesViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Coordinates = (await _coordinatesRequester.GetCoordinatesAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Coordinates/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] CoordinatesViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Coordinates = (await _coordinatesRequester.GetCoordinatesAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Coordinates/Addresses")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _coordinatesRequester.GetNamesCoordinatesAsync(searchString))
+                    .Append(searchString ?? "")
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Coordinates/Regions/Names")]
+        public async Task<JsonResult> RegionsNames([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _regionsRequester.GetRegionsAsync(new RegionsFilters() 
+                    {
+                        ItemsPerPage = 5,
+                        SearchString = searchString
+                    }))
+                    .Select(region => new
+                    {
+                        Id = region.Id,
+                        Text = region.Name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/CoordinatesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class CoordinatesViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public CoordinatesFilters Filters { get; set; }
+
+        public List<Coordinate> Coordinates { get; set; }
+    }
+}
