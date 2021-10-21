244b3940a008e2341097fc18c24c63411362a20f
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-10-21 21:26:10 +0500

    Реализовано отображение сущности "Option" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/OptionsController.cs
@@ -0,0 +1,86 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OptionsController : Controller
+    {
+        private readonly OptionsRequester _optionsRequester;
+
+        public OptionsController()
+        {
+            _optionsRequester = new OptionsRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(OptionsFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _optionsRequester.GetCountOptionsAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/Options")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new OptionsFilters();
+
+            var viewModel = new OptionsViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                Options = (await _optionsRequester.GetOptionsAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Options")]
+        public async Task<PartialViewResult> Index([FromForm] OptionsViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Options = (await _optionsRequester.GetOptionsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Options/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] OptionsViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Options = (await _optionsRequester.GetOptionsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Options/Names")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _optionsRequester.GetNamesOptionsAsync(searchString))
+                    .Append(searchString ?? "")
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/OptionsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class OptionsViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public OptionsFilters Filters { get; set; }
+
+        public List<Option> Options { get; set; }
+    }
+}
