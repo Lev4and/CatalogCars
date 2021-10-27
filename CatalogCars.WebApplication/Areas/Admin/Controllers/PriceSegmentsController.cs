e01fb3a4a38441162d1cb660ec676f3e737ab431
Андрей Левченко <a.levchenko@aviscloud.com>
2021-10-27 10:37:33 +0500

    Реализовано отображение сущности "PriceSegment" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/PriceSegmentsController.cs
@@ -0,0 +1,84 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PriceSegmentsController : Controller
+    {
+        private readonly PriceSegmentsRequester _priceSegmentsRequester;
+
+        public PriceSegmentsController()
+        {
+            _priceSegmentsRequester = new PriceSegmentsRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(PriceSegmentsFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _priceSegmentsRequester.GetCountPriceSegmentsAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/PriceSegments")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new PriceSegmentsFilters();
+
+            var viewModel = new PriceSegmentsViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                PriceSegments = (await _priceSegmentsRequester.GetPriceSegmentsAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/PriceSegments")]
+        public async Task<PartialViewResult> Index([FromForm] PriceSegmentsViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.PriceSegments = (await _priceSegmentsRequester.GetPriceSegmentsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/PriceSegments/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] PriceSegmentsViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.PriceSegments = (await _priceSegmentsRequester.GetPriceSegmentsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/PriceSegments/Names")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _priceSegmentsRequester.GetNamesPriceSegmentsAsync(searchString))
+                    .Append(searchString ?? "")
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/PriceSegmentsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class PriceSegmentsViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public PriceSegmentsFilters Filters { get; set; }
+
+        public List<PriceSegment> PriceSegments { get; set; }
+    }
+}
