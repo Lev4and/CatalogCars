0cf0b919deb090e459b0876afe430ea7772e1ba2
Андрей Левченко <a.levchenko@aviscloud.com>
2021-10-27 10:25:39 +0500

    Реализовано отображение сущности "PhotoClass" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/PhotoClassesController.cs
@@ -0,0 +1,84 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PhotoClassesController : Controller
+    {
+        private readonly PhotoClassesRequester _photoClassesRequester;
+
+        public PhotoClassesController()
+        {
+            _photoClassesRequester = new PhotoClassesRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(PhotoClassesFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _photoClassesRequester.GetCountPhotoClassesAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/PhotoClasses")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new PhotoClassesFilters();
+
+            var viewModel = new PhotoClassesViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                PhotoClasses = (await _photoClassesRequester.GetPhotoClassesAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/PhotoClasses")]
+        public async Task<PartialViewResult> Index([FromForm] PhotoClassesViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.PhotoClasses = (await _photoClassesRequester.GetPhotoClassesAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/PhotoClasses/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] PhotoClassesViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.PhotoClasses = (await _photoClassesRequester.GetPhotoClassesAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/PhotoClasses/Names")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _photoClassesRequester.GetNamesPhotoClassesAsync(searchString))
+                    .Append(searchString ?? "")
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/PhotoClassesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class PhotoClassesViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public PhotoClassesFilters Filters { get; set; }
+
+        public List<PhotoClass> PhotoClasses { get; set; }
+    }
+}
