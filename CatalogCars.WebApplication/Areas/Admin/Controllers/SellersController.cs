cfc605bbd4fd0f25b4f2a4db93268ade48a0fe74
Андрей Левченко <a.levchenko@aviscloud.com>
2021-10-27 12:16:42 +0500

    Реализовано отображение сущности "Seller" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/SellersController.cs
@@ -0,0 +1,107 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SellersController : Controller
+    {
+        private readonly SellersRequester _sellersRequester;
+        private readonly RegionsRequester _regionsRequester;
+
+        public SellersController()
+        {
+            _sellersRequester = new SellersRequester();
+            _regionsRequester = new RegionsRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(SellersFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _sellersRequester.GetCountSellersAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/Sellers")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new SellersFilters();
+
+            var viewModel = new SellersViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                Sellers = (await _sellersRequester.GetSellersAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Sellers")]
+        public async Task<PartialViewResult> Index([FromForm] SellersViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Sellers = (await _sellersRequester.GetSellersAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Sellers/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] SellersViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Sellers = (await _sellersRequester.GetSellersAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Sellers/Names")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _sellersRequester.GetNamesSellersAsync(searchString))
+                    .Append(searchString ?? "")
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Sellers/Regions/Names")]
+        public async Task<JsonResult> RegionsNames([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _regionsRequester.GetRegionsAsync(new RegionsFilters()
+                {
+                    ItemsPerPage = 5,
+                    SearchString = searchString
+                }))
+                    .Select(region => new
+                    {
+                        Id = region.Id,
+                        Text = region.Name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/SellersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class SellersViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public SellersFilters Filters { get; set; }
+
+        public List<Seller> Sellers { get; set; }
+    }
+}
