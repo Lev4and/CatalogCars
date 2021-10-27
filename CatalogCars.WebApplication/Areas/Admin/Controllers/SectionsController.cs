71999cfba6868a946105f5f66b6224fd46a6eca5
Андрей Левченко <a.levchenko@aviscloud.com>
2021-10-27 12:02:27 +0500

    Реализовано отображение сущности "Section" в MVC.

---
+++ CatalogCars.WebApplication/Areas/Admin/Controllers/SectionsController.cs
@@ -0,0 +1,84 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Resource.Requests.HttpRequesters;
+using CatalogCars.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogCars.WebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SectionsController : Controller
+    {
+        private readonly SectionsRequester _sectionsRequester;
+
+        public SectionsController()
+        {
+            _sectionsRequester = new SectionsRequester();
+        }
+
+        private async Task<Pagination> GetPaginationAsync(SectionsFilters filters)
+        {
+            return new Pagination()
+            {
+                NumberPage = filters.NumberPage,
+                ItemsPerPage = filters.ItemsPerPage,
+                CountTotalItems = await _sectionsRequester.GetCountSectionsAsync(filters)
+            };
+        }
+
+        [HttpGet]
+        [Route("~/Admin/Sections")]
+        public async Task<IActionResult> Index()
+        {
+            var filters = new SectionsFilters();
+
+            var viewModel = new SectionsViewModel()
+            {
+                Filters = filters,
+                Pagination = await GetPaginationAsync(filters),
+                Sections = (await _sectionsRequester.GetSectionsAsync(filters)).ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Sections")]
+        public async Task<PartialViewResult> Index([FromForm] SectionsViewModel viewModel)
+        {
+            viewModel.Filters.NumberPage = 1;
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Sections = (await _sectionsRequester.GetSectionsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Sections/page={page}")]
+        public async Task<PartialViewResult> Index([FromForm] SectionsViewModel viewModel, [FromRoute] int page)
+        {
+            viewModel.Filters.NumberPage = page;
+            viewModel.Pagination = await GetPaginationAsync(viewModel.Filters);
+            viewModel.Sections = (await _sectionsRequester.GetSectionsAsync(viewModel.Filters)).ToList();
+
+            return PartialView("_Table", viewModel);
+        }
+
+        [HttpPost]
+        [Route("~/Admin/Sections/Names")]
+        public async Task<JsonResult> Names([FromForm] string searchString)
+        {
+            return Json(new
+            {
+                results = (await _sectionsRequester.GetNamesSectionsAsync(searchString))
+                    .Append(searchString ?? "")
+                    .Select(name => new
+                    {
+                        Id = name,
+                        Text = name
+                    })
+            });
+        }
+    }
+}
---
+++ CatalogCars.WebApplication/Models/SectionsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CatalogCars.Model.Database.AuxiliaryTypes;
+using CatalogCars.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace CatalogCars.WebApplication.Models
+{
+    public class SectionsViewModel
+    {
+        public Pagination Pagination { get; set; }
+
+        public SectionsFilters Filters { get; set; }
+
+        public List<Section> Sections { get; set; }
+    }
+}
