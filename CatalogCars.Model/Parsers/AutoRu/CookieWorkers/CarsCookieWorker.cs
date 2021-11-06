05b1c4f24efc423a2ca590b94b98109c7a50e4c2
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-11-06 13:13:18 +0500

    Исправление багов.

--- CatalogCars.Model/Parsers/AutoRu/CookieWorkers/CarsCookieWorker.cs
+++ CatalogCars.Model/Parsers/AutoRu/CookieWorkers/CarsCookieWorker.cs
@@ -34,26 +34,19 @@ namespace CatalogCars.Model.Parsers.AutoRu.CookieWorkers
 
         public async Task<HeadersAjaxRequestForCars> GetHeadersAjaxRequestForCars(RangeMileage rangeMileage, RangePrice rangePrice, int pageSize, int topDays, int numberPage)
         {
-            try
+            var result = new HeadersAjaxRequestForCars();
+            var cookies = await _cookieLoader.GetCookies(rangeMileage, rangePrice, pageSize, topDays, numberPage);
+
+            result.CsrfToken = cookies.FirstOrDefault(cookie => cookie.Name == "_csrf_token")?.Value ?? "";
+
+            foreach (var cookie in cookies)
             {
-                var result = new HeadersAjaxRequestForCars();
-                var cookies = await _cookieLoader.GetCookies(rangeMileage, rangePrice, pageSize, topDays, numberPage);
-
-                result.CsrfToken = cookies.FirstOrDefault(cookie => cookie.Name == "_csrf_token").Value;
-
-                foreach (var cookie in cookies)
-                {
-                    result.CookieContent += $"{cookie.Name}={cookie.Value};";
-                }
-
-                result.CookieContent += "gradius=1000;gids=";
-
-                return result;
+                result.CookieContent += $"{cookie.Name}={cookie.Value};";
             }
-            catch
-            {
-                return new HeadersAjaxRequestForCars();
-            }
+
+            result.CookieContent += "gradius=1000;gids=";
+
+            return result;
         }
     }
 }
